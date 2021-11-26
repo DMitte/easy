4d80fa6f13b7ae576775112fb371f9d3402ed9d7
DMitte <mittedany547@gmail.com>
2021-11-25 21:38:09 -0500

    header terminado

---
+++ js/app.js
@@ -0,0 +1,6 @@
+function abrir(){
+    let nav = document.querySelector('#header');
+    let divi = document.querySelector('.divicion');
+    nav.classList.toggle("active1");
+    divi.classList.toggle('show');
+}
